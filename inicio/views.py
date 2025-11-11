9b00be6bd840e7928e2279e11fca913ad6e23584
Dayra Lizeth <dayra24564@cbtis75.edu.mx>
2025-11-11 16:10:33 -0600

    diseño pt1

--- inicio/views.py
+++ inicio/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'inicio/home.html', {'titulo': 'PAEC - Inicio', 'mensaje': 'Bienvenido a PAEC!'})
-
-def about(request):
-    return render(request, 'inicio/about.html', {'titulo': 'PAEC - Acerca de', 'mensaje': 'Conoce más sobre PAEC.'})
+    return render(request, 'home.html', {'titulo': 'Bienvenido a PAEC', 'mensaje': 'Aprendizaje y renovación para todos.'})
 
 def servicios(request):
-    return render(request, 'inicio/servicios.html', {'titulo': 'PAEC - Servicios', 'mensaje': 'Estos son nuestros servicios.'})
+    return render(request, 'servicios.html', {'titulo': 'Nuestros Servicios'})
 
 def contacto(request):
-    return render(request, 'inicio/contacto.html', {'titulo': 'PAEC - Contacto', 'mensaje': 'Contáctanos aquí.'})
+    return render(request, 'contacto.html', {'titulo': 'Contacto'})
+
+def about(request):
+    return render(request, 'about.html', {'titulo': 'Acerca de Nosotros'})
+
