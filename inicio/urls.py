e3e668400c18b21c56eedad687b63c82a59b9c7e
Evelin241 <evelin24561@cbtis75.edu.mx>
2025-11-28 17:34:58 -0600

    no jalo

--- inicio/urls.py
+++ inicio/urls.py
@@ -3,9 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
-    path('servicios/', views.servicios, name='servicios'),
-    path('contacto/', views.contacto, name='contacto'),
 ]
-
-x
