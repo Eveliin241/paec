3638ad6472a0171957e0c97f65d9a9bb6b651608
Evelin241 <evelin24561@cbtis75.edu.mx>
2025-11-28 16:37:03 -0600

    ya que se suba

--- inicio/views.py
+++ inicio/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 
+
+def contacto(request):
+    return render(request, 'inicio/index.html')
+
 def home(request):
     return render(request, 'inicio/home.html')
 
 def about(request):
-    return render(request, 'inicio/about.html')
+    return render(request, 'inicio/base.html')
 
 def servicios(request):
     return render(request, 'inicio/servicios.html')
 
-def contacto(request):
-    return render(request, 'inicio/contacto.html')
